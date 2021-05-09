171cb4c7d93033ba8034b380c71df27524a08bc2
mollyisaac <54511777+mollyisaac@users.noreply.github.com>
2021-05-09 16:05:47 -0500

    Add files via upload

---
+++ cli/CMakeFiles/openafis-cli.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fuzzusers/openafis/cli/Main.cpp" "/home/fuzzusers/openafis/cli/CMakeFiles/openafis-cli.dir/Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_STEADY_CLOCK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/fuzzusers/openafis/lib/CMakeFiles/openafis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cli/CMakeFiles/openafis-cli.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openafis-cli.dir/Main.cpp.o"
+  "openafis-cli.pdb"
+  "openafis-cli"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openafis-cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
